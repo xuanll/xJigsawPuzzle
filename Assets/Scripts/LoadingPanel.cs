176f82f36d9bdbb5f51ef321ea4f53a509af7fe1
xuanll <xuanll@yeah.net>
2015-12-31 13:40:47 +0800

    init - loading scene and jigsaw scene

---
+++ Assets/Scripts/LoadingPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		UIEventListener.Get (GameObject.Find ("Start")).onClick = OnStartClicked;
+	}
+
+
+	//Start Button Click function
+	void OnStartClicked(GameObject go)
+	{
+		//Load jigsaw scene
+		Application.LoadLevel ("jigsaw");
+	}
+}
---
+++ Assets/Scripts/MainPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		UIEventListener.Get (GameObject.Find ("Back")).onClick = OnBackClicked;
+	}
+
+	void OnBackClicked(GameObject go)
+	{
+		//Load Loading scene
+		Application.LoadLevel ("Loading");
+	}
+
+}
